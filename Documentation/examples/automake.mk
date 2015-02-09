f4e799f6240a93839ac8fa535915bbc16a41cef5
Simon Horman <horms@verge.net.au>
2015-02-09 13:20:37 +0900

    Documentation: Add missing json files to automake.mk
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- Documentation/examples/automake.mk
+++ Documentation/examples/automake.mk
@@ -1,4 +1,8 @@
 EXTRA_DIST += \
 	Documentation/examples/del-flows.json \
+	Documentation/examples/actions.json \
 	Documentation/examples/flows.json \
+	Documentation/examples/header-graph.json \
+	Documentation/examples/headers.json \
+	Documentation/examples/table-graph.json \
 	Documentation/examples/tables.json
